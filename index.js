b041775652eaa2e6e304426ad3a8c25624671b63
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-07-01 22:13:05 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const electron = require('electron');
+const path = require('path');
+const { app, BrowserWindow, Tray } = electron;
+const TimerTray = require('./app/timer_tray');
+const mainWindow = require('./app/main_window');
+
+let mainWindow;
+let tray;
+
+app.on('ready', () => {
+    app.dock.hide();
+    mainWindow = new mainWindow({
+        height: 600,
+        width: 400,
+        resizable: false,
+        frame: false
+    }, `file://${__dirname}/src/index.html`);
+
+    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
+    const iconPath = path.join(__dirname , '.src/assets/${iconName}');
+
+    tray = new TimerTray(iconPath);
+
+})
