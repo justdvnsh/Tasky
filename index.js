ce5e8fc9ead6d396017066869e350f015d3006fd
Divyansh Dwivedi <justdvnsh2208@gmail.com>
2018-07-02 01:11:18 +0530

    minor bug fixes

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-const electron = require('electron');
 const path = require('path');
-const { app, BrowserWindow, Tray, ipcMain } = electron;
+const electron = require('electron');
 const TimerTray = require('./app/timer_tray');
-const mainWindow = require('./app/main_window');
+const MainWindow = require('./app/main_window');
+
+const { app, ipcMain } = electron;
 
 let mainWindow;
 let tray;
 
 app.on('ready', () => {
-    app.dock.hide();
-    mainWindow = new mainWindow({
-        height: 600,
-        width: 400,
-        resizable: false,
-        frame: false
-    }, `file://${__dirname}/src/index.html`);
+  //app.dock.hide();
+  mainWindow = new MainWindow(`file://${__dirname}/src/index.html`);
 
-    const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png'
-    const iconPath = path.join(__dirname , '.src/assets/${iconName}');
-
-    tray = new TimerTray(iconPath);
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+  tray = new TimerTray(iconPath, mainWindow);
 });
 
-ipcMain.on('update-timer', ( event, timeLeft ) => {
-    tray.setTitle(timeLeft);
-})
+ipcMain.on('update-timer', (event, timeLeft) => {
+  tray.setTitle(timeLeft);
+});
