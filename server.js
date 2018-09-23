884d4ac9a62957eab8a2e5ff52bec77c872a6102
Glitch (one-time-logger) <none>
2018-09-23 17:09:32 +0000

    🚙⏱ Updated with Glitch - github login

--- server.js
+++ server.js
@@ -16,6 +16,28 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+/**
+ * @name Github
+ *
+ * @desc Logs into Github. Provide your username and password as environment variables when running the script, i.e:
+ * `GITHUB_USER=myuser GITHUB_PWD=mypassword node github.js`
+ *
+ */
+const puppeteer = require('puppeteer')
+const screenshot = 'github.png';
+(async () => {
+  const browser = await puppeteer.launch({headless: true})
+  const page = await browser.newPage()
+  await page.goto('https://github.com/login')
+  await page.type('#login_field', process.env.GITHUB_USER)
+  await page.type('#password', process.env.GITHUB_PWD)
+  await page.click('[name="commit"]')
+  await page.waitForNavigation()
+  await page.screenshot({ path: screenshot })
+  browser.close()
+  console.log('See screenshot: ' + screenshot)
+})()
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
