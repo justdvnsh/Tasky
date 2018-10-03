5b46af8c839d4443dc410e5785331f80b17cae04
Glitch (one-time-logger) <none>
2018-10-03 09:22:05 +0000

    😨📒 Updated with Glitch

--- server.js
+++ server.js
@@ -5,8 +5,25 @@
 var express = require('express');
 var app = express();
 
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+const mongoDB = process.env.MLAB_URI;
+
+mongoose.connect(mongoDB);
+const db = mongoose.connection;
+db.on('error', (err) => { console.log('Mongo DB connection error', err); });
+db.once('open', () => { console.log('Mongo DB connected.'); });
+
+app.use(cors({ optionSuccessStatus: 200 }));  // some legacy browsers choke on 204
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -36,6 +53,21 @@ const screenshot = 'github.png';
   await page.screenshot({ path: screenshot })
   browser.close()
   console.log('See screenshot: ' + screenshot)
+})()
+
+
+const screenshot2 = 'twitter.png';
+(async () => {
+  const browser = await puppeteer.launch({headless: true})
+  const page = await browser.newPage()
+  await page.goto('https://twitter.com/login')
+  await page.type('#login_field', process.env.TWITTER_USER)
+  await page.type('#password', process.env.TWITTER_PWD)
+  await page.click('[name="commit"]')
+  await page.waitForNavigation()
+  await page.screenshot({ path: screenshot2 })
+  browser.close()
+  console.log('See screenshot: ' + screenshot2)
 })()
 
 // listen for requests :)
